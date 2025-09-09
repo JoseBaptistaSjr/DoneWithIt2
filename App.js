953af4db6129daf2b891ccbc1e07f509951ca4f6
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-09 20:22:20 -0300

    Layout

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import AppButton from './app/components/AppButton';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
+export default function App() {
+  return (
+     <ViewImageScreen/>
+  );
+}
+
+
+
