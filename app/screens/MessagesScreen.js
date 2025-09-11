34c09e77c4fceae5774284abce6f52d5b47be057
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-10 21:37:38 -0300

    handling selections

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -22,6 +22,7 @@ function MessagesScreen(props) {
                 title={item.title}
                 subTitle={item.description}
                 image={item.image}
+                onPress={() => console.log("Message selected", item)}
               />}
               ItemSeparatorComponent={ListItemSeparator}
             />
